71fd9a9498a676f796622bbbcb7cbc567ba0ca78
suveshgurung <suveshgurung2@gmail.com>
2023-11-19 16:17:18 +0545

    server set up

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// express app
+const app = express();
+
+// environment variables
+const mongoUri = process.env.MONGO_URI;
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(mongoUri)
+    .then(() => {
+        console.log("Connected to database");
+
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        })
+    })
+    .catch((error) => {
+        console.error(error);
+    });
