0622711eabbbe776243930a9fc9624505f1fccc7
suveshgurung <suveshgurung2@gmail.com>
2023-11-19 20:33:30 +0545

    user model updated for some refinements

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,19 +6,24 @@ const userSchema = new Schema({
     username: {
         type: String,
         required: true,
+        trim: true,
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        trim: true,
     },
     password: {
         type: String,
         required: true,
+        trim: true,
+        minLength: 8,
     },
     user_photo: {
         type: String,
         default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHXi6kWCo1P3qJAuOnEAs6jWS1Dg1BqRkk8Q&usqp=CAU',
+        trim: true,
     }
 }, {
     timestamps: true,
